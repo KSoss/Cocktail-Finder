4e12f879ea53ce1732d52fa17232e87c7a9cc82a
Never404s <kdsossa@gmail.com>
2023-06-21 21:22:41 -0500

    repition from ingredients minimized

--- front.js
+++ front.js
@@ -1,33 +1,46 @@
-
 // function getDrinkInfo(search) {
 let $displayArea = $(".card-container")
-let search = 'rum'
 
-
-// ready document. Using main search button on click to run main function
 $(document).ready(function(){
+    // Using main search button on click to run main function
     $( "#btn" ).click(function() {     
-        console.log( ".click called." );
         getDrinkInfo($("#search-text").val())
     });
-})
 
-// ready document. Using main search bar on enter to be able to search. 
-$(document).ready(function(){
+    // Using main search bar on enter to be able to search. 
     $('input').keypress(function(event){
         var keycode = (event.keyCode ? event.keyCode : event.which);
+        //if statement for enter button on keyboard
         if(keycode == '13'){
             getDrinkInfo($("#search-text").val())
         }     
-      });
-})
-
-$(document).ready(function(){
+    });
+    // Using random button for extra fun!
     $( "#btnR" ).click(function() {     
-        console.log( ".click called." );
         getRandomDrink()
     });
-})
+});
+
+
+function fetchIngredientsAndMeasures(drink) {
+    const ingredientRaw = [];
+    const measureRaw = [];
+
+    // Loop through the drink object to fetch ingredients and measures
+    for (let i = 1; i <= 10; i++) {
+        if (drink[`strIngredient${i}`]) {
+            ingredientRaw.push(drink[`strIngredient${i}`]);
+        }
+        if (drink[`strMeasure${i}`]) {
+            measureRaw.push(drink[`strMeasure${i}`]);
+        }
+    }
+    // Remove undefined values
+    const ingredient = ingredientRaw.filter(x => x);
+    const measure = measureRaw.filter(x => x);
+
+    return { ingredient, measure };
+}
 
 
 // Main function for search for drinks via typing in the search bar
@@ -41,23 +54,14 @@ $.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + search, (dat
         let $pic = $('<img></img>').addClass("error")
         $pic.attr('src', 'https://blog.expertrec.com/wp-content/uploads/2021/01/no-result-found-1200x675.jpg')
         $(".card-container").append($pic)
-        return console.log('no results :(')
+        return console.log('no results 😢') 
     }
-
+    
     for (let i = 0; i < data.drinks.length; i++) {
         let drinkTitle = data.drinks[i].strDrink 
         let drinkPicture = data.drinks[i].strDrinkThumb
-        
-
-        // best attempt for ingredients. API is formated weird.
-        var ingredientRaw = [data.drinks[i].strIngredient1,data.drinks[i].strIngredient2,data.drinks[i].strIngredient3,data.drinks[i].strIngredient4,data.drinks[i].strIngredient5,data.drinks[i].strIngredient6,data.drinks[i].strIngredient7,data.drinks[i].strIngredient8,data.drinks[i].strIngredient9,data.drinks[i].strIngredient10]
-        var measureRaw = [data.drinks[i].strMeasure1,data.drinks[i].strMeasure2,data.drinks[i].strMeasure3,data.drinks[i].strMeasure4,data.drinks[i].strMeasure5,data.drinks[i].strMeasure6,data.drinks[i].strMeasure7,data.drinks[i].strMeasure8,data.drinks[i].strMeasure9,data.drinks[i].strMeasure10]
-        
-        ingredient = ingredientRaw.filter(x => x)
-        measure = measureRaw.filter(x => x)
-
         var instructions = data.drinks[i].strInstructions
-
+        const { ingredient, measure } = fetchIngredientsAndMeasures(data.drinks[i]);
         pushToPage(drinkTitle, drinkPicture, ingredient, measure, instructions)
     }
 
@@ -70,24 +74,13 @@ function getRandomDrink() {
     $.get("https://www.thecocktaildb.com/api/json/v1/1/random.php", (data) => {
         console.log(data.drinks);
         $(".card-container").empty()
-    
 
             let drinkTitle = data.drinks[0].strDrink 
             let drinkPicture = data.drinks[0].strDrinkThumb
-            
-    
-            var ingredientRaw = [data.drinks[0].strIngredient1,data.drinks[0].strIngredient2,data.drinks[0].strIngredient3,data.drinks[0].strIngredient4,data.drinks[0].strIngredient5,data.drinks[0].strIngredient6,data.drinks[0].strIngredient7,data.drinks[0].strIngredient8,data.drinks[0].strIngredient9,data.drinks[0].strIngredient10]
-            var measureRaw = [data.drinks[0].strMeasure1,data.drinks[0].strMeasure2,data.drinks[0].strMeasure3,data.drinks[0].strMeasure4,data.drinks[0].strMeasure5,data.drinks[0].strMeasure6,data.drinks[0].strMeasure7,data.drinks[0].strMeasure8,data.drinks[0].strMeasure9,data.drinks[0].strMeasure10]
-            
-            ingredient = ingredientRaw.filter(x => x)
-            measure = measureRaw.filter(x => x)
-    
-            var instructions = data.drinks[0].strInstructions
-    
+            let instructions = data.drinks[0].strInstructions
+            const { ingredient, measure } = fetchIngredientsAndMeasures(data.drinks[0]);
             pushToPage(drinkTitle, drinkPicture, ingredient, measure, instructions)
-    
-    
-    
+
     });
     
 }
